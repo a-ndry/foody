3ccce0d3853105245e050cf962b0e725bb318cea
Andry <andrysitrakiniainasoa@gmail.com>
2022-06-29 07:47:37 +0300

    Add food menu

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import About from "./components/About/About";
 import Header from "./components/Header/Header";
+import Menu from "./components/Menu/Menu";
 import Navbar from "./components/Navbar/Navbar";
 import Openings from "./components/Openings/Openings";
 import Services from "./components/Services/Services";
@@ -13,6 +14,7 @@ function App() {
       <About />
       <Services />
       <Openings />
+      <Menu />
     </div>
   );
 }
---
+++ client/src/components/Menu/Menu.tsx
@@ -0,0 +1,93 @@
+import { Button, Container, Card, Row, Col } from "react-bootstrap";
+import Separator from "../Separator/Separator";
+import SectionHeading from "../Typography/SectionHeading";
+import breakfast from "../../assets/images/food-breakfast.jpg";
+import chicken from "../../assets/images/food-chicken.jpg";
+import pizza from "../../assets/images/food-pizza.jpg";
+import lunch from "../../assets/images/food-lunch.jpg";
+import dinner from "../../assets/images/food-dinner.jpg";
+import burger from "../../assets/images/food-burger.jpg";
+
+interface IMenu {
+  title: string;
+  description: string;
+  src: string;
+  price: number;
+}
+
+const menus: IMenu[] = [
+  {
+    title: "Breakfast",
+    description: "Lorem ipsum dolor sit amet consectetur.",
+    src: breakfast,
+    price: 10,
+  },
+  {
+    title: "chicken",
+    description: "Lorem ipsum dolor sit amet consectetur.",
+    src: chicken,
+    price: 10,
+  },
+  {
+    title: "lunch",
+    description: "Lorem ipsum dolor sit amet consectetur.",
+    src: lunch,
+    price: 10,
+  },
+  {
+    title: "pizza",
+    description: "Lorem ipsum dolor sit amet consectetur.",
+    src: pizza,
+    price: 10,
+  },
+  {
+    title: "dinner",
+    description: "Lorem ipsum dolor sit amet consectetur.",
+    src: dinner,
+    price: 10,
+  },
+  {
+    title: "burger",
+    description: "Lorem ipsum dolor sit amet consectetur.",
+    src: burger,
+    price: 10,
+  },
+];
+
+const Menu = () => {
+  return (
+    <div id="menu" className="py-5">
+      <Container className="pt-5 pb-4 px-lg-5 px-xl-0">
+        <SectionHeading>Food Menu</SectionHeading>
+        <Separator />
+        <Row xs={1} lg={2} className="mt-5 mb-3">
+          {menus.map((menu) => (
+            <Col className="mb-3" key={menu.title}>
+              {makeMenuCard(menu)}
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+const makeMenuCard = (menuDetail: IMenu) => {
+  return (
+    <Card className="flex-row border-0 align-items-center gap-3">
+      <Card.Img src={menuDetail.src} style={{ width: "50%" }} />
+      <Card.Body>
+        <Card.Title className="text-secondary d-flex justify-content-between gap-3">
+          <span className="type text-capitalize">{menuDetail.title}</span>
+          <span className="price">${menuDetail.price}</span>
+        </Card.Title>
+        <Card.Text>{menuDetail.description}</Card.Text>
+        <Button variant="danger" className="rounded-0">
+          Order now
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Menu;
